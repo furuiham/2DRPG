e875b2d2ec62c3a34c8958bcc237a8983254f6d4
furuihan <qqpasswordlost@outlook.com>
2024-05-27 17:16:23 +0800

    <feature> 野猪的撞墙逻辑

--- 勇士传说/Assets/Script/Enemy/Enemy.cs
+++ 勇士传说/Assets/Script/Enemy/Enemy.cs
@@ -8,18 +8,33 @@ public class Enemy : MonoBehaviour {
     [Header("基本参数")]
     public float normalSpeed;
 
+    public PhysicsCheck physicsCheck;
+
     public float chaseSpeed;
     public float currentSpeed;
     public Vector3 faceDir;
 
+    [Header("计时器")]
+    public float waitTime;
+    public bool wait;
+    public float waitTimeCounter;
     public void Awake() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         currentSpeed = normalSpeed;
+        physicsCheck = GetComponent<PhysicsCheck>();
+        currentSpeed = normalSpeed;
+        waitTimeCounter = waitTime;
     }
 
     private void Update() {
-        faceDir = new Vector3(-transform.localScale.x, 0, 0);
+        faceDir = new Vector3(-transform.localScale.x, 0f, 0f);
+
+        if ((physicsCheck.touchLeftWall && faceDir.x < 0f) || (physicsCheck.touchRightWall && faceDir.x > 0f)) {
+            wait = true;
+            anim.SetBool("walk", false);
+        }
+        TimeCounter();
     }
 
     public void FixedUpdate() {
@@ -28,5 +43,16 @@ public class Enemy : MonoBehaviour {
 
     public virtual void Move() {
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void TimeCounter() {
+        if (wait) {
+            waitTimeCounter -= Time.deltaTime;
+            if (waitTimeCounter <= 0) {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
     }
 }
